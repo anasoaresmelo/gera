2812a042d4a37c86fe4fffbbb4f2c48bce83af24
Ana Melo <anacsoaresmelo@gmail.com>
2021-02-26 09:07:45 -0300

    Inicia projeto

---
+++ Gera-App/Gera_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gera_AppApp.swift
+//  Gera-App
+//
+//  Created by Ana Carolina Soares de Melo on 26/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Gera_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
