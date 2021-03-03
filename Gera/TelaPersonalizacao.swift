d8675cbb9dc02679385379d3505054e8013545b1
Ana Melo <anacsoaresmelo@gmail.com>
2021-03-03 09:59:46 -0300

    Atualiza botões e mensagem

--- Gera/TelaPersonalizacao.swift
+++ Gera/TelaPersonalizacao.swift
@@ -41,70 +41,73 @@ struct TelaPersonalizacao: View {
                         .padding(.top)
                     Spacer()
                 }
-                HStack {
-                    Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+                VStack {
+                    HStack {
+                        Button(action: {}) {
+                            Text("Tô mandando a conta!")
+                        }
+                        .padding(.leading)
+                        .padding(.top, 10)
+                        Spacer()
                     }
-                    .padding(.leading)
-                    .padding(.top, 10)
-                    Spacer()
-                }
-                HStack {
-                    Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+                    HStack {
+                        Button(action: {}) {
+                            Text("Você me deve!")
+                        }
+                        .padding(.leading)
+                        .padding(.top, 10)
+                        Spacer()
                     }
-                    .padding(.leading)
-                    .padding(.top, 10)
-                    Spacer()
-                }
-                HStack {
-                    Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+                    HStack{
+                        Button(action: {}) {
+                            Text("Pague meu dinheiro!")
+                        }
+                        .padding(.leading)
+                        .padding(.top, 10)
+                        Spacer()
                     }
-                    .padding(.leading)
-                    .padding(.top, 10)
-                    Spacer()
                 }
+                .font(.system(size: 16, weight: .regular, design: .default))
+                .foregroundColor(Color(.systemPurple))
             }.padding(.horizontal)
             
             // MARK: Cor
             Group {
                 HStack {
                     Text("Cores")
-                        .font(.body)
-                        .fontWeight(.bold)
+                        .font(.system(size: 20, weight: .bold, design: .default))
                         .padding(.horizontal)
                         .padding(.top)
                     Spacer()
                 }
                 HStack {
                     Button(action: {}) {
-                    RoundedRectangle(cornerRadius: 10.0, style: .continuous)
-                        .foregroundColor(Color(.systemPurple))
+                        RoundedRectangle(cornerRadius: 10.0, style: .continuous)
+                            .foregroundColor(Color(.systemPurple))
                     }
                     Button(action: {}) {
-                    RoundedRectangle(cornerRadius: 10.0, style: .continuous)
-                        .foregroundColor(Color(.systemRed))
+                        RoundedRectangle(cornerRadius: 10.0, style: .continuous)
+                            .foregroundColor(Color(.systemRed))
                     }
                     Button(action: {}) {
-                    RoundedRectangle(cornerRadius: 10.0, style: .continuous)
-                        .foregroundColor(Color(.systemOrange))
+                        RoundedRectangle(cornerRadius: 10.0, style: .continuous)
+                            .foregroundColor(Color(.systemOrange))
                     }
                     Button(action: {}) {
-                    RoundedRectangle(cornerRadius: 10.0, style: .continuous)
-                        .foregroundColor(Color(.systemYellow))
+                        RoundedRectangle(cornerRadius: 10.0, style: .continuous)
+                            .foregroundColor(Color(.systemYellow))
                     }
                     Button(action: {}) {
-                    RoundedRectangle(cornerRadius: 10.0, style: .continuous)
-                        .foregroundColor(Color(.systemGreen))
+                        RoundedRectangle(cornerRadius: 10.0, style: .continuous)
+                            .foregroundColor(Color(.systemGreen))
                     }
                     Button(action: {}) {
-                    RoundedRectangle(cornerRadius: 10.0, style: .continuous)
-                        .foregroundColor(Color(.systemBlue))
+                        RoundedRectangle(cornerRadius: 10.0, style: .continuous)
+                            .foregroundColor(Color(.systemBlue))
                     }
                     Button(action: {}) {
-                    RoundedRectangle(cornerRadius: 10.0, style: .continuous)
-                        .foregroundColor(Color(.systemGray))
+                        RoundedRectangle(cornerRadius: 10.0, style: .continuous)
+                            .foregroundColor(Color(.systemGray))
                     }
                 }
                 .frame(width: UIScreen.main.bounds.width/1.2, height: 45)
@@ -112,7 +115,7 @@ struct TelaPersonalizacao: View {
             
             // MARK: Continuar
             BotaoContinuar()
-                .padding(.top)
+                .padding(.top, 20)
         }
     }
 }
