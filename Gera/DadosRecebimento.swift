4231d9c8f729a67f7b4ce4e6a91d55b604543040
Íris Soares <iss3@cin.ufpe.br>
2021-03-03 19:20:19 -0300

    cria tela de dados de recebimento

---
+++ Gera/DadosRecebimento.swift
@@ -0,0 +1,231 @@
+//
+//  SwiftUIView.swift
+//  Gera
+//
+//  Created by iris on 03/03/21.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    
+    @State var selected = 0
+    @State var documento = 0
+    @State var tipoConta = 0
+    @State var valor = ""
+    @State var codigoBanco = ""
+    @State var nomeBanco = ""
+    @State var agencia = ""
+    @State var conta = ""
+    @State var titular = ""
+    @State var cpfCnpj = ""
+    @State var urlNubank = ""
+    @State var userPicPay = ""
+    @State var linhaDigitavel = ""
+    
+    var body: some View {
+        VStack{
+            Text("Por fim, adicione os\ndados de recebimento")
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .padding(.bottom)
+            
+            HStack{
+                Text("Forma de recebimento")
+                    .fontWeight(.bold)
+                    .padding(.leading)
+                Spacer()
+            }.padding(.horizontal)
+            
+            Picker(selection: $selected, label: Text(""), content: {
+                Text("Conta").tag(0)
+                Text("Nubank").tag(1)
+                Text("PicPay").tag(2)
+                Text("Boleto").tag(3)
+            }).padding(.horizontal).pickerStyle(SegmentedPickerStyle())
+            .padding(.horizontal)
+            
+            if(selected == 0) {
+                Group{
+                    Group{
+                        Divider()
+                        HStack{
+                            Text("Valor")
+                            TextField("Opicional", text: $valor)
+                        }
+                        Divider()
+                        HStack{
+                            Text("Código do Banco")
+                            TextField("Obrigatório", text: $codigoBanco)
+                        }
+                        Divider()
+                        HStack{
+                            Text("Nome do Banco")
+                            TextField("Obrigatório", text: $nomeBanco)
+                        }
+                        Divider()
+                        HStack{
+                            Text("Agência")
+                            TextField("Obrigatório", text: $agencia)
+                        }
+                        Divider()
+                        HStack{
+                            Text("Conta")
+                            TextField("Obrigatório", text: $conta)
+                        }
+                    }.padding(.horizontal)
+                    Group{
+                        Divider()
+                        HStack{
+                            Text("Titular")
+                            TextField("Obrigatório", text: $titular)
+                        }
+                        Divider()
+                        HStack{
+                            Text("Documento")
+                            Picker(selection: $documento, label: Text(""), content: {
+                                Text("CPF").tag(0)
+                                Text("CNPJ").tag(1)
+                            }).pickerStyle(SegmentedPickerStyle())
+                        }
+                        Divider()
+                        HStack{
+                            if(documento == 0){
+                                Text("CPF")
+                            }
+                            else {
+                                Text("CNPJ")
+                            }
+                            TextField("Obrigatório", text: $cpfCnpj)
+                        }
+                        Divider()
+                        HStack{
+                            Text("Tipo de Conta")
+                            Picker(selection: $tipoConta, label: Text(""), content: {
+                                Text("Corrente").tag(0)
+                                Text("Poupança").tag(1)
+                            }).pickerStyle(SegmentedPickerStyle())
+                        }
+                        Divider()
+                        
+                    }.padding(.horizontal)
+                }.padding(.horizontal)
+            }
+            else if(selected == 1) {
+                Group{
+                    Group{
+                        Divider()
+                        HStack{
+                            Text("Valor")
+                            TextField("Opicional", text: $valor)
+                        }
+                        Divider()
+                        HStack{
+                            Text("URL do Nubank")
+                            TextField("Obrigatório", text: $urlNubank)
+                        }
+                        Divider()
+                        HStack{
+                            Text("Conta")
+                            TextField("Obrigatório", text: $conta)
+                        }
+                        Divider()
+                        HStack{
+                            Text("Titular")
+                            TextField("Obrigatório", text: $titular)
+                        }
+                    }.padding(.horizontal)
+                    Group{
+                        Divider()
+                        HStack{
+                            Text("Documento")
+                            Picker(selection: $documento, label: Text(""), content: {
+                                Text("CPF").tag(0)
+                                Text("CNPJ").tag(1)
+                            }).pickerStyle(SegmentedPickerStyle())
+                        }
+                        Divider()
+                        HStack{
+                            if(documento == 0){
+                                Text("CPF")
+                            }
+                            else {
+                                Text("CNPJ")
+                            }
+                            TextField("Obrigatório", text: $valor)
+                        }
+                        Divider()
+                    }.padding(.horizontal)
+                }.padding(.horizontal)
+            }
+            else if(selected == 2) {
+                Group{
+                    Divider()
+                    HStack{
+                        Text("Valor")
+                        TextField("Opicional", text: $valor)
+                    }
+                    Divider()
+                    HStack{
+                        Text("Usuário")
+                        TextField("Obrigatório", text: $userPicPay)
+                    }
+                    Divider()
+                }.padding(.horizontal)
+                .padding(.horizontal)
+            }
+            else if(selected == 3) {
+                Group{
+                    Group{
+                        Divider()
+                        HStack{
+                            Text("Valor")
+                            TextField("Obrigatório", text: $valor)
+                        }
+                        Divider()
+                        HStack{
+                            Text("Linha digitável")
+                            TextField("Obrigatório", text: $linhaDigitavel)
+                        }
+                        Divider()
+                        HStack{
+                            Text("Titular")
+                            TextField("Obrigatório", text: $titular)
+                        }
+                    }.padding(.horizontal)
+                    Group{
+                        Divider()
+                        HStack{
+                            Text("Documento")
+                            Picker(selection: $documento, label: Text(""), content: {
+                                Text("CPF").tag(0)
+                                Text("CNPJ").tag(1)
+                            }).pickerStyle(SegmentedPickerStyle())
+                        }
+                        Divider()
+                        HStack{
+                            if(documento == 0){
+                                Text("CPF")
+                            }
+                            else {
+                                Text("CNPJ")
+                            }
+                            TextField("Obrigatório", text: $valor)
+                        }
+                        Divider()
+                    }.padding(.horizontal)
+                }.padding(.horizontal)
+            }
+            Spacer()
+            BotaoGerarCartao()
+                .padding(.bottom)
+        }
+        .padding(.vertical)
+    }
+}
+
+struct DadosRecebimento_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
